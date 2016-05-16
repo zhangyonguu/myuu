e38cfbc9c04a0b5a9374e296029e18c9ba796ef3
zhangyonguu <651751676@qq.com>
2016-05-16 11:18:16 +0800

    test
    test

---
+++ aaaa/aaaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aaaa
+//
+//  Created by tarena on 16/5/16.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
